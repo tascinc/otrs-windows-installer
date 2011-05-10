0c821596f22ac738f7333005d939024b814a1793
Michiel Beijen <michiel.beijen@otrs.com>
2011-05-10 12:15:09 +0000

    Added support for installation of OTRS 3.1 Scheduler. Shebang line replacement is not needed when running as mod_perl.

--- Scripts/ConfigureOTRS.pl
+++ Scripts/ConfigureOTRS.pl
@@ -1,9 +1,9 @@
 #!/usr/bin/perl -w
 # --
 # ConfigureOTRS.pl - script to configure OTRS
-# Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
+# Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
 # --
-# $Id: ConfigureOTRS.pl,v 1.10 2010-10-26 08:20:17 mb Exp $
+# $Id: ConfigureOTRS.pl,v 1.11 2011-05-10 12:15:09 mb Exp $
 # --
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU AFFERO General Public License as published by
@@ -29,7 +29,7 @@ use File::Copy;
 use File::Find;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.10 $) [1];
+$VERSION = qw($Revision: 1.11 $) [1];
 
 # get options
 my %Opts = ();
@@ -41,7 +41,7 @@ if ( !$Opts{'d'} ) {
 }
 if ( $Opts{'h'} ) {
     print STDOUT "ConfigureOTRS.pl <Revision $VERSION> - script to configure OTRS\n";
-    print STDOUT "Copyright (C) 2001-2010 OTRS AG, http://otrs.org/\n";
+    print STDOUT "Copyright (C) 2001-2011 OTRS AG, http://otrs.org/\n";
     print STDOUT "usage: ConfigureOTRS.pl -d <install directory>\n\n";
     exit 1;
 }
@@ -74,9 +74,6 @@ CreateConfigPm();
 # create GenericAgent.pm file
 CreateGenericAgentPm();
 
-# set shebangline with the correct perl directory in all .pl files
-find( \&ReplaceShebangLine, ($OTRSDir) );
-
 # set directory to OTRS in the config files
 ReplaceOTRSDir();
 
@@ -85,6 +82,17 @@ PrepareConfigPm();
 
 # config the Cron4Win32.pl
 ConfigCron4Win32Pl();
+
+# Configure the OTRS Scheduler service, only if it's present (OTRS 3.1 and up)
+
+if ( -e "$OTRSDirQuoted/bin/otrs.Scheduler4win.pl" ) {
+
+    # config the OTRS server start and restart scripts
+    ConfigOTRSServiceStart();
+
+    # config the OTRS server stop and restart scripts
+    ConfigOTRSServiceStop();
+}
 
 1;
 
@@ -298,10 +306,11 @@ sub ConfigCron4Win32Pl {
 
     # insert configuration
     $NewString
-        =~ s{(my \$PerlExe   = ")(";)}{$1$InstallDirQuoted/StrawberryPerl/perl/bin/perl.exe$2};
-    $NewString =~ s{(my \$Directory = ")(";)}{$1$OTRSDirQuoted/var/cron/$2};
-    $NewString =~ s{(my \$CronTab   = ")(";)}{$1$InstallDirQuoted/CRONw/crontab.txt$2};
-    $NewString =~ s{(my \$OTRSHome  = ")(";)}{$1$OTRSDirQuoted$2};
+        =~ s{(my \$PerlExe\s*= ")(";)}{$1$InstallDirQuoted/StrawberryPerl/perl/bin/perl.exe$2};
+    $NewString =~ s{(my \$Directory\s*= ")(";)}{$1$OTRSDirQuoted/var/cron/$2};
+    $NewString =~ s{(my \$CronTab\s*= ")(";)}{$1$InstallDirQuoted/CRONw/crontab.txt$2};
+    $NewString =~ s{(my \$CronTabFile\s*= ")(";)}{$1$InstallDirQuoted/CRONw/crontab.txt$2};
+    $NewString =~ s{(my \$OTRSHome\s*= ")(";)}{$1$OTRSDirQuoted$2};
 
     # return if no changes
     return 1 if $OrgString eq $NewString;
@@ -312,6 +321,104 @@ sub ConfigCron4Win32Pl {
     close $FH2;
 
     return 1;
+}
+
+sub ConfigOTRSServiceStart {
+
+    FILE:
+    for my $FileName (qw(OTRSServicesStart.bat OTRSServicesRestart.bat)) {
+
+        # add install directory
+        my $File = $InstallDirQuoted . '/otrs4win/Scripts/' . $FileName;
+
+        # check if file exists
+        next FILE if !-e $File;
+
+        # check if file is a directory
+        next FILE if -d $File;
+
+        # check if file is writeable
+        next FILE if !-w $File;
+
+        # check if file is a link
+        next FILE if -l $File;
+
+        # check if file is a text file
+        next FILE if !-T $File;
+
+        # read file
+        next FILE if !open my $FH1, '<', $File;
+        my $OrgString = do { local $/; <$FH1> };
+        close $FH1;
+
+        # copy the string
+        my $NewString = $OrgString;
+
+        my $StartConfig = "REM Start OTRS Scheduler service
+\"$InstallDirQuoted/StrawberryPerl/perl/bin/perl.exe\" \"$InstallDirQuoted/OTRS/bin/otrs.Scheduler4win.pl\" -a start";
+
+        # add the OTRS Scheduler start part
+        $NewString =~ s{ ^ REM \s ---OTRSSchedulerStartPart--- }{$StartConfig}xmsg;
+
+        # next file if no changes
+        next FILE if $OrgString eq $NewString;
+
+        # write new file
+        return if !open my $FH2, '>', $File;
+        print $FH2 $NewString;
+        close $FH2;
+
+        print STDERR "Replaced string 'REM ---OTRSSchedulerStartPart---' in $File\n";
+    }
+}
+
+sub ConfigOTRSServiceStop {
+
+    FILE:
+    for my $FileName (qw(OTRSServicesStop.bat OTRSServicesRestart.bat)) {
+
+        # add install directory
+        my $File = $InstallDirQuoted . '/otrs4win/Scripts/' . $FileName;
+
+        # check if file exists
+        next FILE if !-e $File;
+
+        # check if file is a directory
+        next FILE if -d $File;
+
+        # check if file is writeable
+        next FILE if !-w $File;
+
+        # check if file is a link
+        next FILE if -l $File;
+
+        # check if file is a text file
+        next FILE if !-T $File;
+
+        # read file
+        next FILE if !open my $FH1, '<', $File;
+        my $OrgString = do { local $/; <$FH1> };
+        close $FH1;
+
+        # copy the string
+        my $NewString = $OrgString;
+
+        my $StopConfig = "REM Stop OTRS Scheduler service
+\"$InstallDirQuoted/StrawberryPerl/perl/bin/perl.exe\" \"$InstallDirQuoted/OTRS/bin/otrs.Scheduler4win.pl\" -a stop";
+
+        # add the OTRS Scheduler stop part
+        $NewString =~ s{ ^ REM \s ---OTRSSchedulerStopPart--- }{$StopConfig}xmsg;
+
+        # next file if no changes
+        next FILE if $OrgString eq $NewString;
+
+        # write new file
+        return if !open my $FH2, '>', $File;
+        print $FH2 $NewString;
+        close $FH2;
+
+        print STDERR "Replaced string 'REM ---OTRSSchedulerStopPart---' in $File\n";
+    }
 }
 
 exit 0;
