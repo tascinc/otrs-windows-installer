d5d239105898c8203f5a97a572a1522a5fcb9338
Michiel Beijen <michiel.beijen@otrs.com>
2013-02-08 16:03:52 +0000

    No longer configure batch files.

--- Scripts/ConfigureMySQL.pl
+++ Scripts/ConfigureMySQL.pl
@@ -1,9 +1,9 @@
 #!/usr/bin/perl -w
 # --
 # ConfigureMySQL.pl - script to configure MySQL
-# Copyright (C) 2001-2012 OTRS AG, http://otrs.org/
+# Copyright (C) 2001-2013 OTRS AG, http://otrs.org/
 # --
-# $Id: ConfigureMySQL.pl,v 1.7 2012-11-20 19:18:27 mh Exp $
+# $Id: ConfigureMySQL.pl,v 1.8 2013-02-08 16:03:52 mb Exp $
 # --
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU AFFERO General Public License as published by
@@ -29,7 +29,7 @@ use File::Copy;
 use File::Find;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.7 $) [1];
+$VERSION = qw($Revision: 1.8 $) [1];
 
 # get options
 my %Opts = ();
@@ -41,7 +41,7 @@ if ( !$Opts{'d'} ) {
 }
 if ( $Opts{'h'} ) {
     print STDOUT "ConfigureMySQL.pl <Revision $VERSION> - script to configure MySQL\n";
-    print STDOUT "Copyright (C) 2001-2012 OTRS AG, http://otrs.org/\n";
+    print STDOUT "Copyright (C) 2001-2013 OTRS AG, http://otrs.org/\n";
     print STDOUT "usage: ConfigureMySQL.pl -d <install directory>\n\n";
     exit 1;
 }
@@ -73,12 +73,6 @@ CreateMyIni();
 
 # set required parameters in my.ini
 PrepareMyIni();
-
-# config the OTRS server start and restart scripts
-ConfigOTRSServiceStart();
-
-# config the OTRS server stop and restart scripts
-ConfigOTRSServiceStop();
 
 1;
 
@@ -146,104 +140,6 @@ query_cache_type = 1}xms;
     close $FH2;
 
     return 1;
-}
-
-sub ConfigOTRSServiceStart {
-
-    FILE:
-    for my $FileName (qw(OTRSServicesStart.bat OTRSServicesRestart.bat)) {
-
-        # add install directory
-        my $File = $InstallDirQuoted . '/otrs4win/Scripts/' . $FileName;
-
-        # check if file exists
-        next FILE if !-e $File;
-
-        # check if file is a directory
-        next FILE if -d $File;
-
-        # check if file is writeable
-        next FILE if !-w $File;
-
-        # check if file is a link
-        next FILE if -l $File;
-
-        # check if file is a text file
-        next FILE if !-T $File;
-
-        # read file
-        next FILE if !open my $FH1, '<', $File;
-        my $OrgString = do { local $/; <$FH1> };
-        close $FH1;
-
-        # copy the string
-        my $NewString = $OrgString;
-
-        my $StartConfig = "REM Start MySQL service
-NET START MySQL";
-
-        # add the mysql start part
-        $NewString =~ s{ ^ REM \s ---MySQLStartPart--- }{$StartConfig}xmsg;
-
-        # next file if no changes
-        next FILE if $OrgString eq $NewString;
-
-        # write new file
-        return if !open my $FH2, '>', $File;
-        print $FH2 $NewString;
-        close $FH2;
-
-        print STDERR "Replaced string 'REM ---MySQLStartPart---' in $File\n";
-    }
-}
-
-sub ConfigOTRSServiceStop {
-
-    FILE:
-    for my $FileName (qw(OTRSServicesStop.bat OTRSServicesRestart.bat)) {
-
-        # add install directory
-        my $File = $InstallDirQuoted . '/otrs4win/Scripts/' . $FileName;
-
-        # check if file exists
-        next FILE if !-e $File;
-
-        # check if file is a directory
-        next FILE if -d $File;
-
-        # check if file is writeable
-        next FILE if !-w $File;
-
-        # check if file is a link
-        next FILE if -l $File;
-
-        # check if file is a text file
-        next FILE if !-T $File;
-
-        # read file
-        next FILE if !open my $FH1, '<', $File;
-        my $OrgString = do { local $/; <$FH1> };
-        close $FH1;
-
-        # copy the string
-        my $NewString = $OrgString;
-
-        my $StopConfig = "REM Stop MySQL service
-NET STOP MySQL";
-
-        # add the mysql stop part
-        $NewString =~ s{ ^ REM \s ---MySQLStopPart--- }{$StopConfig}xmsg;
-
-        # next file if no changes
-        next FILE if $OrgString eq $NewString;
-
-        # write new file
-        return if !open my $FH2, '>', $File;
-        print $FH2 $NewString;
-        close $FH2;
-
-        print STDERR "Replaced string 'REM ---MySQLStopPart---' in $File\n";
-    }
 }
 
 exit 0;
